b2bda3978d88c8b65e3af138aa1794210885df60
Antonio Boccia <antonioboccia737@gmail.com>
2026-01-26 13:01:59 +0100

    auto commit

--- autopush.sh
+++ autopush.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 git add .
-git commit -m "Auto update"
-git push
+
+if git diff --cached --quiet; then
+  echo "Niente da committare."
+  exit 0
+fi
+
+git commit -m "auto commit"
+git pull --rebase origin main
+git push origin main
